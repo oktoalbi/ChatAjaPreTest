d181b942e3f6fdfbb4268df745a4bf6d5e83b72e
oktoalbi <67061158+oktoalbi@users.noreply.github.com>
2020-06-17 23:19:40 +0700

    Create KeyboardFragment.java

---
+++ KeyboardFragment.java
@@ -0,0 +1,33 @@
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
+import android.view.View;
+
+public class KeyboardFragment extends BottomSheetDialogFragment {
+
+    private BottomSheetBehavior.BottomSheetCallback
+            mBottomSheetBehaviorCallback = new BottomSheetBehavior.BottomSheetCallback() {
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss();
+            }
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+        }
+    };
+
+    @Override
+    public void setupDialog(final Dialog dialog, int style) {
+        super.setupDialog(dialog, style);
+        View contentView = View.inflate(getContext(), R.layout.fragment_bottomsheet3, null);
+        dialog.setContentView(contentView);
+    }
+
+}
